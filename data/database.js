fea8cd84b8f9e1c6d9b2dd7aef526cd92423b79c
khurrum qureshi <khurrum.qureshi@goodcoresoft.com>
2013-05-06 11:34:04 +0500

    add more branches

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ module.exports.database = {
     name:"Standard Chartered Singapore",
     address:"7 Changi Business Park Crescent Singapore 486028",
     tel:"1800 747 7000",
-    email:"",
+    email:"info@standardchartered.com.sg",
     web:"http://www.standardchartered.com.sg/",
     location:{
         lat:1.334303,
@@ -13,6 +13,7 @@ module.exports.database = {
             name:"Plaza Singapura (with Priority Banking Centre)",
             address:"68 Orchard Road #04-53 Plaza Singapura S238839",
             tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
             atm_available:true,
             location:{
                 lat:1.300614,
@@ -38,6 +39,7 @@ module.exports.database = {
             name:"Mandarin Gallery (Priority Banking Centre)",
             address:"333a Orchard Road #01-18A & #03-13, Singapore 238867",
             tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
             atm_available:true,
             location:{
                 lat:1.302589,
@@ -63,6 +65,7 @@ module.exports.database = {
             name:"Upper Thomson",
             address:"246R Upper Thomson Road Singapore 574370",
             tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
             atm_available:true,
             location:{
                 lat:1.353631,
@@ -88,6 +91,7 @@ module.exports.database = {
             name:"Serangoon Garden",
             address:"Serangoon Garden 67 Serangoon Garden Way Singapore 555963",
             tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
             atm_available:false,
             location:{
                 lat:1.363814,
@@ -113,6 +117,7 @@ module.exports.database = {
             name:"Battery Road",
             address:"6 Battery Road Singapore 049909, Battery Road, Central",
             tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
             atm_available:false,
             location:{
                 lat:1.285539,
@@ -138,10 +143,115 @@ module.exports.database = {
             name:"Tampines Branch",
             address:"3 Tampines Central 1, #01-01, Abacus Plaza Singapore 529540",
             tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
             atm_available:true,
             location:{
                 lat:1.352323,
                 long:103.942678
+            },
+            contacts:[
+                {
+                    name:"Yi Ling Joseph",
+                    designation:"Branch Manager",
+                    tel:"1800 747 7000",
+                    email:"yi@scb.com.sg"
+                },
+                {
+                    name:"Jasmine Darryl",
+                    designation:"Customer Sales Representative",
+                    tel:"1800 747 7000",
+                    email:"jasmine@scb.com.sg"
+                }
+            ]
+
+        },
+        {
+            name:"Marine Parade Branch",
+            address:"Blk 87 Marine Parade Central #01-500 Singapore 440087",
+            tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
+            atm_available:true,
+            location:{
+                lat:1.302638,
+                long:103.905569
+            },
+            contacts:[
+                {
+                    name:"Yi Ling Joseph",
+                    designation:"Branch Manager",
+                    tel:"1800 747 7000",
+                    email:"yi@scb.com.sg"
+                },
+                {
+                    name:"Jasmine Darryl",
+                    designation:"Customer Sales Representative",
+                    tel:"1800 747 7000",
+                    email:"jasmine@scb.com.sg"
+                }
+            ]
+
+        },
+        {
+            name:"Battery Road",
+            address:"6 Battery Road Singapore 049909",
+            tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
+            atm_available:true,
+            location:{
+                lat:1.285539,
+                long:103.851667
+            },
+            contacts:[
+                {
+                    name:"Yi Ling Joseph",
+                    designation:"Branch Manager",
+                    tel:"1800 747 7000",
+                    email:"yi@scb.com.sg"
+                },
+                {
+                    name:"Jasmine Darryl",
+                    designation:"Customer Sales Representative",
+                    tel:"1800 747 7000",
+                    email:"jasmine@scb.com.sg"
+                }
+            ]
+
+        },
+        {
+            name:"Woodlands Branch",
+            address:"900 South Woodlands Drive #02-07 Woodlands Civic Centre Singapore 730900",
+            tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
+            atm_available:true,
+            location:{
+                lat:1.440232,
+                long:103.770550
+            },
+            contacts:[
+                {
+                    name:"Yi Ling Joseph",
+                    designation:"Branch Manager",
+                    tel:"1800 747 7000",
+                    email:"yi@scb.com.sg"
+                },
+                {
+                    name:"Jasmine Darryl",
+                    designation:"Customer Sales Representative",
+                    tel:"1800 747 7000",
+                    email:"jasmine@scb.com.sg"
+                }
+            ]
+
+        },
+        {
+            name:"Mandarin Gallery (Priority Banking Centre)",
+            address:"333a Orchard Road #01-18A & #03-13, Singapore 238867",
+            tel:"1800 747 7000",
+            operating_hours:"Mon to Fri: 11am to 7pm",
+            atm_available:true,
+            location:{
+                lat:1.302589,
+                long:103.836118
             },
             contacts:[
                 {
